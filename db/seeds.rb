5400d9dde359a73eb5821e3774bbb7f9b3d19cef
jimmy littmann <littmann.j@gmail.com>
2016-09-18 19:31:21 +0200

    avatar pictures homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-la_mer = Flat.create(name:'La mer', description:'1 bedroom with balcony')
-le_port = Flat.create(name:'Le port', description:'2 bedrooms with balcony')
+la_mer = Flat.create(name:'La mer', description:'1 bedroom with balcony', picture:'http://res.cloudinary.com/dvgxgsxyc/image/upload/v1474219211/la-mer_supa6y.png')
+le_port = Flat.create(name:'Le port', description:'2 bedrooms with balcony', picture:'http://res.cloudinary.com/dvgxgsxyc/image/upload/v1474219269/le-port_ilxryb.png')
 
 la_mer.save!
 le_port.save!
